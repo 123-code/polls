0653e061f364706279f9cd3fbea60e62ffbb63fd
joseignacionaranjo <jose.naranjo.martinez@udla.edu.ec>
2024-11-29 11:24:13 -0500

    cambios

--- controllers/Candidates.go
+++ controllers/Candidates.go
@@ -12,17 +12,17 @@ import (
 func (vc *VoteController) GetAllVotes(c *gin.Context) {
     var votes []models.MyVote
 
-    // Get sorting order from query parameters
-    sortOrder := c.Query("sort")
-    fmt.Println("Sort parameter received:", sortOrder) // Debugging log
 
-    // Default to sorting by newest if no sort order is specified
-    order := "created_at DESC" // Newest first
+    sortOrder := c.Query("sort")
+    fmt.Println("Sort parameter received:", sortOrder) 
+
+
+    order := "created_at DESC" 
     if sortOrder == "oldest" {
-        order = "created_at ASC" // Oldest first
+        order = "created_at ASC" 
     }
 
-    // Query all votes and sort by the specified order
+
     if err := initializers.DB.Model(&models.MyVote{}).Order(order).Find(&votes).Error; err != nil {
         c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch votes"})
         return
